e7621d172cc3df4631ab0c55eae46a768e35f9fb
imhilla <hillaryodhiambo282@gmail.com>
2020-07-29 18:01:20 +0300

    add test for inverse of in users model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe User, type: :model do
     it { should have_many(:posts) }
     it { should have_many(:comments).dependent(:destroy) }
     it { should have_many(:likes).dependent(:destroy) }
-    it { should have_many(:recieved_friendship).class_name('Friendship').with_foreign_key('reciever_user_id')}
-    it { should have_many(:requested_friendship).class_name('Friendship').with_foreign_key('requester_user_id')}
+    it { should have_many(:recieved_friendship).class_name('Friendship').with_foreign_key('reciever_user_id').inverse_of(:reciever_user)}
+    it { should have_many(:requested_friendship).class_name('Friendship').with_foreign_key('requester_user_id').inverse_of(:requester_user)}
   end
 
 end
