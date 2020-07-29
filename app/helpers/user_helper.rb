54e8e4c6d340f0d58892654df5988d650811ab06
ozovalihasan <ozovalihasan@gmail.com>
2020-07-29 16:53:11 +0300

    Move helper methods of User to user model and add confirmed_friend_request method

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,51 +1,6 @@
 module UserHelper
-  def friends(user)
-    friends = []
-    Friendship.all.each do |request|
-    	if request.status == true 
-    		if request.requester_user_id == user.id
-    			friends << User.find( request.reciever_user_id )
-    		elsif request.reciever_user_id == user.id 
-    			friends << User.find(request.requester_user_id)
-    		end
-    	end
-    end
-    friends
-  end
+ 
 
-  def pending_requests(user)
-    pending_friend = []
-    Friendship.all.each do |pending|
-      if pending.status == nil
-        if pending.reciever_user_id == user.id
-          pending_friend << pending
-        end
-      end
-    end
-    pending_friend
-  end
 
-  def friend_requests(user)
-    requests = []
-    Friendship.all.each do |request|
-      if request.status == nil
-        if request.requester_user_id == user.id
-          requests << request
-        end
-      end
-    end
-    requests
-  end
-
-  def accept_friend_request(friendship_request)
-    friendship_request.update(status: true)     
-  end
-
-  def reject_friend_request(friendship_request)
-    friendship_request.update(status: false)     
-  end
-
-  def friend?(user,user2)
-    friends(user).include?(user2)
-  end
+ 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,69 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :recieved_friendship, class_name: 'Frienship', foreign_key: 'reciever_user_id', inverse_of: :reciever_userz #inverse of friendship
   has_many :requested_friendship, class_name: 'Friendship', foreign_key: 'requester_user_id', inverse_of: :requester_user#friendship
+
+def friends
+    friends = []
+    Friendship.all.each do |request|
+      if request.status == true 
+        if request.requester_user_id == self.id
+          friends << User.find( request.reciever_user_id )
+        elsif request.reciever_user_id == self.id 
+          friends << User.find(request.requester_user_id)
+        end
+      end
+    end
+    friends
+  end
+
+  def pending_requests
+    pending_friend = []
+    Friendship.all.each do |pending|
+      if pending.status == nil
+        if pending.reciever_user_id == self.id
+          pending_friend << pending
+        end
+      end
+    end
+    pending_friend
+  end
+
+  def friend_requests
+    requests = []
+    Friendship.all.each do |request|
+      if request.status == nil
+        if request.requester_user_id == self.id
+          requests << request
+        end
+      end
+    end
+    requests
+  end
+
+  def accept_friend_request(user)
+    friendship_request = Friendship.find_by(requester_user_id: user.id,reciever_user_id: self.id, status: nil)
+    friendship_request.update(status: true)     
+  end
+
+  def reject_friend_request(user)
+    friendship_request = Friendship.find_by(requester_user_id: user.id,reciever_user_id: self.id, status: nil)
+    friendship_request.update(status: false)     
+  end
+
+  def confirmed_friend_request
+    requests = []
+    Friendship.all.each do |request|
+      if request.status == true 
+        if request.reciever_user_id == self.id
+          requests << request
+        end
+      end
+    end
+    requests
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
 end
