972b91b756c568de2d7a870e7137e8c90deae23c
ozovalihasan <ozovalihasan@gmail.com>
2020-08-07 12:24:05 +0300

    Move database from postgresql to sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_07_28_145927) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
@@ -26,8 +23,8 @@ ActiveRecord::Schema.define(version: 2020_07_28_145927) do
   end
 
   create_table "friendships", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "friend_id"
+    t.integer "user_id"
+    t.integer "friend_id"
     t.boolean "confirmed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -64,6 +61,4 @@ ActiveRecord::Schema.define(version: 2020_07_28_145927) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "friendships", "users"
-  add_foreign_key "friendships", "users", column: "friend_id"
 end
