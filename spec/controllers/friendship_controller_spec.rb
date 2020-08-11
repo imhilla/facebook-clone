16decd793b663f4c770c0950e1e9d0c5ca10920f
imhilla <hillaryodhiambo282@gmail.com>
2020-08-11 12:03:00 +0300

    run rubocop to fix errors

--- spec/controllers/friendship_controller_spec.rb
+++ spec/controllers/friendship_controller_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-
-RSpec.describe FriendshipsController, :type => :controller do
+RSpec.describe FriendshipsController, type: :controller do
   before(:all) do
     User.create(name: 'hasan', email: 'ozovalihasan@gmail.com', password: '123456')
     User.create(name: 'hillary', email: 'hillary@gmail.com', password: '123456')
     User.first.friendships.create(friend_id: User.second.id)
   end
 
-  describe "#edit" do
-    it "creates two friendships for the same two users" do
-      expect {
-        post :edit, params: {id: 1}
-      }.to change(Friendship, :count).by(1)
+  describe '#edit' do
+    it 'creates two friendships for the same two users' do
+      expect do
+        post :edit, params: { id: 1 }
+      end.to change(Friendship, :count).by(1)
     end
   end
 end
